b4e44ccf3775c75d18221e4e87a0ea6f31bd48fb
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-09-04 15:56:58 +0530

    Routing part completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,23 +9,19 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-  selectedHero :Hero ;
-  
+
   heroes: Hero[];
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
   
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   constructor(
     private heroService: HeroService,
   ) {
 
-   }
+  }
 
   ngOnInit() {
     this.getHeroes();
